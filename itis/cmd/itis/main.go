07cd22c24856f26da155fc0e8111b696de88aa58
Гафиятуллин Дамир <damir.gafiyatullin@akbarsdigital.ru>
2021-04-27 15:52:59 +0300

    Сборка/релиз выполнение

--- itis/cmd/itis/main.go
+++ itis/cmd/itis/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
+	"net/http"
 	"os"
 )
 
@@ -13,4 +14,11 @@ func main()  {
 	if len(port) == 0{
 		logrus.Fatal("Port is not set")
 	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//w.WriteHeader(http.StatusOK)
+		w.Write([]byte("This is a sample Page."))
+	})
+
+	http.ListenAndServe(":"+ port, nil)
 }
