bf8c9cb315df6c191570916e135f3b73bb6ed12a
Гафиятуллин Дамир <damir.gafiyatullin@akbarsdigital.ru>
2021-04-27 14:54:31 +0300

    Фактор 3. Конфигурирование.

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
 func main()  {
-	
+	logrus.Info("Hello world")
+
+	var port = os.Getenv("PORT")
+
+	if len(port) == 0{
+		logrus.Fatal("Port is not set")
+	}
 }
